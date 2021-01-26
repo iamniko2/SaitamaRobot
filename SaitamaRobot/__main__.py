6060afb58f5c077fcee532099e771b73fc5d2d53
iamnikott <76602846+iamnikott@users.noreply.github.com>
2021-01-26 05:34:44 +0400

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -214,7 +214,7 @@ def start(update: Update, context: CallbackContext):
                      ]]))
     else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>"
+            "Salam Nezaret Bot Xidmətinizdədi!"
             .format(uptime),
             parse_mode=ParseMode.HTML)
 

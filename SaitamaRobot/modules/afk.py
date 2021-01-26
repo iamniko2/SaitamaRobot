05067da6498b291ebebdeb045d97a2200f5248ad
iamnikott <76602846+iamnikott@users.noreply.github.com>
2021-01-27 02:08:55 +0400

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -29,7 +29,7 @@ def afk(update: Update, context: CallbackContext):
         reason = args[1]
         if len(reason) > 100:
             reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+            notice = "\n100 Herfden Çox Olmaz!"
     else:
         reason = ""
 
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
-                '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {}', 'Where is {}?\nIn the chat!'
+                '{} Qayıtdı!', '{} Geldi!', '{} Yenıden Aramızdadı!',
+                '{} Darıxmışdıq!', '{} Yeniden Onlinedir!', '{} Artıq Qayıtdı!',
+                'Xoş Geldin! {}', 'Seni Axtarırdıq! {}?\nSohbetimize Yeniden Geldi!'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -102,7 +102,7 @@ def reply_afk(update: Update, context: CallbackContext):
                 try:
                     chat = bot.get_chat(user_id)
                 except BadRequest:
-                    print("Error: Could not fetch userid {} for AFK module"
+                    print("Xeta: Could not fetch userid {} for AFK module"
                           .format(user_id))
                     return
                 fst_name = chat.first_name
@@ -129,7 +129,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} Afkdır.\nReason: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason))
             update.effective_message.reply_text(res, parse_mode="html")
 

8edae984c4f875ed7cd5c97ccbcf474ae9118925
iamnikott <76602846+iamnikott@users.noreply.github.com>
2021-01-27 09:32:50 +0400

    Update gtranslator.py

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -90,10 +90,10 @@ def totranslate(update: Update, context: CallbackContext):
 
 
 __help__ = """
-• `/tr` or `/tl` (language code) as reply to a long message
-*Example:* 
-  `/tr en`*:* translates something to english
-  `/tr hi-en`*:* translates hindi to english
+• /tr və ya /tl (dil kodu) yanıt verilən mesajı tərcümə edir
+Nümunə: 
+  /tr en: İngilis dilinə tərcümə edir
+  /tr en-ru: İngilis dilindən Rus dilinə tərcümə edir
 """
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)

32f6f478a45902c09d7d4da69f518556a5803626
iamnikott <76602846+iamnikott@users.noreply.github.com>
2021-01-27 10:22:56 +0400

    Update muting.py

--- SaitamaRobot/modules/muting.py
+++ SaitamaRobot/modules/muting.py
@@ -223,9 +223,9 @@ def temp_mute(update: Update, context: CallbackContext) -> str:
 
 __help__ = """
 *Admins only:*
- • `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
- • `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+• /mute <userhandle>: bir istifadəçini susdurur. İstifadəçiyə verilən cavabı susduraraq cavab olaraq da istifadə edilə bilər.
+  • /tmute <userhandle> x (m / h / d): istifadəçini x müddətə səssiz edir. (sap və ya cavab vasitəsilə). m = dəqiqə, h = saat, d = gün.
+  • /unmute <userhandle>: bir istifadəçinin səsini açır. İstifadəçiyə verilən cavabı susduraraq cavab olaraq da istifadə edilə bilər. 
 """
 
 MUTE_HANDLER = CommandHandler("mute", mute)
@@ -236,5 +236,5 @@ dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 
-__mod_name__ = "Muting"
+__mod_name__ = "Susdurma"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]

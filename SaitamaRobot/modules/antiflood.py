28b7494bd7b7c39cf3a820563b84e6b4393c0304
iamnikott <76602846+iamnikott@users.noreply.github.com>
2021-01-27 08:11:58 +0400

    Update antiflood.py

--- SaitamaRobot/modules/antiflood.py
+++ SaitamaRobot/modules/antiflood.py
@@ -334,27 +334,15 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
-will result in restricting that user.
+ - /flood: Flood haqqında məlumat
 
- This will mute users if they send more than 10 messages in a row, bots are ignored.
- • `/flood`*:* Get the current flood control setting
-
-• *Admins only:*
- • `/setflood <int/'no'/'off'>`*:* enables or disables flood control
- *Example:* `/setflood 10`
- • `/setfloodmode <ban/kick/mute/tban/tmute> <value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
-
-• *Note:*
- • Value must be filled for tban and tmute!!
- It can be:
- `5m` = 5 minutes
- `6h` = 6 hours
- `3d` = 3 days
- `1w` = 1 week
+Admin only:
+ - /setflood <rəqəm/'no'/'off'>: flood yandırmaq və ya flood rəqəmin təyin etmək
+ Misal üçün: /setflood 10
+ Əgər istifadəçi 10-dan artıq mesaj atsa səssizə alınacaq.
  """
 
-__mod_name__ = "Anti-Flood"
+__mod_name__ = "AntiFlood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
